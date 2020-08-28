82e22745fd3168922227195f32092184131f2fd9
Smekto4ka <barsalekcandr@mail.ru>
2020-08-28 18:20:22 +0300

    Comid

---
+++ HashMap/src/com/hash/HashMap.java
@@ -0,0 +1,70 @@
+package com.hash;
+
+import sun.awt.image.ImageWatched;
+
+import java.util.LinkedList;
+
+import static java.lang.Math.abs;
+
+public class HashMap<K, V> {
+    private int length;
+    private LinkedList<Kay>[] mass;
+    private V removeV;
+    private Kay removeKay;
+
+    public HashMap(int length) {
+        this.length = length;
+        mass = new LinkedList[length];
+        for (int i = 0; i < length; i++) {
+            LinkedList<Kay> list = new LinkedList<>();
+            mass[i]=list;
+        }
+    }
+
+    public void put(K kay, V value) {
+        if (0 < size(kay)) {
+            for (Object k : mass[id(kay)]) {
+                if (((Kay) k).getKay().equals(kay)) {
+                    ((Kay) k).setData(value);
+                    return;
+                }
+            }
+        }
+        mass[id(kay)].add(new Kay<K, V>(kay, value));
+
+    }
+
+    public V get(K kay) {
+        if (0 != size(kay)) {
+            for (Object k : mass[id(kay)])
+                if (kay.equals(((Kay) k).getKay())) {
+                    return (V) ((Kay) k).getValue();
+                }
+        }
+        System.out.print(" нет такого объекта");
+        return null;
+    }
+
+    public int size(K kay) {
+        return (mass[id(kay)]).size();
+    }
+
+    private int id(K kay) {
+        return  abs(kay.hashCode()) % length;
+    }
+
+    public V remove(K kay) {
+        if (0 != size(kay)) {
+            for (int i = 0; i < size(kay); i++) {
+                removeKay = (Kay) mass[id(kay)].get(i);
+                if (kay.equals(removeKay.getKay())) {
+                    removeV = (V) removeKay.getValue();
+                    mass[id(kay)].remove(i);
+                    return removeV;
+                }
+            }
+        }
+        return null;
+    }
+
+}
---
+++ HashMap/src/com/hash/Main.java
@@ -0,0 +1,24 @@
+package com.hash;
+
+public class Main {
+
+    public static void main(String[] args) {
+        HashMap<String, String> hashMap = new HashMap<>(3);
+
+        hashMap.put("asdfgg", "789456123");
+        hashMap.put("qwert", "7417");
+        hashMap.put("asdfgg", "78921");
+        hashMap.put("qsdfgm", "88");
+        hashMap.put("[lkjbvc]", "753");
+
+      System.out.println(hashMap.remove("asdfgg"));
+        System.out.println(hashMap.remove("qwert"));
+        System.out.println(hashMap.remove("qsdfgm"));
+
+
+
+
+
+
+    }
+}
